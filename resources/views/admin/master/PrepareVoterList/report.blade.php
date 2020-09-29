bf71e2c950544e2466afa489f44986b903e56d07
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-09-29 22:33:38 +0530

    Update report.blade.php

--- resources/views/admin/master/PrepareVoterList/report.blade.php
+++ resources/views/admin/master/PrepareVoterList/report.blade.php
@@ -6,13 +6,13 @@
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
 	<style> 
 		body{
-		  font-family: 'Mangal';
+		  font-family: 'MANGAL';
 		  font-style: 'bold';
 		  font-weight: 'bold';
 		  src: url('https://github.com/nsisodiya/rajiv-bharat/raw/master/fonts/MANGAL.ttf') format('truetype');
 		}
 		@font-face {
-		  font-family: 'Mangal';
+		  font-family: 'MANGAL';
 		  font-style: 'bold';
 		  font-weight: 'bold';
 		  src: url('https://github.com/nsisodiya/rajiv-bharat/raw/master/fonts/MANGAL.ttf') format('truetype');
